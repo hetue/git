dc6099057926856cb46936b7dc49027128dc99d7
storezhang <storezhang@gmail.com>
2025-05-08 11:32:48 +0800

    refactor(项目): 基于harluo/boot重构

--- internal/internal/config/credential.go
+++ internal/internal/config/credential.go
@@ -2,9 +2,9 @@ package config
 
 type Credential struct {
 	// 用户名
-	Username string `default:"${NETRC_USERNAME}" json:"username,omitempty"`
+	Username string `default:"${NETRC_USERNAME=${GIT_USERNAME}}" json:"username,omitempty"`
 	// 密码
-	Password string `default:"${NETRC_PASSWORD}" json:"password,omitempty"`
+	Password string `default:"${NETRC_PASSWORD=${GIT_PASSWORD}}" json:"password,omitempty"`
 	// 密钥
 	Key string `json:"key,omitempty"`
 }
